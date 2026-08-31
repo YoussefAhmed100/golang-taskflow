3de841678bbcbbe2fd9bc9faf0df9afa8acbc229
YoussefAhmed100 <youssefahmed16110@gmail.com>
2026-09-01 02:00:36 +0300

    test: verify ci failure protection

--- tests/task/service_test.go
+++ tests/task/service_test.go
@@ -106,4 +106,6 @@ func TestService_Create_MultipleTasks(t *testing.T) {
 	assert.NotZero(t, createdSecond.ID)
 	assert.NotEqual(t, createdFirst.ID, createdSecond.ID)
 }
-
+func TestTemporaryFailure(t *testing.T) {
+	t.Fatal("intentional CI failure")
+}
